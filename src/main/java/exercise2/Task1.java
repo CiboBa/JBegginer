59bdc0ddae9b03d30913f30e7f94bcde853e55fa
Bartek Cib <bciborowski.pl@gmail.com>
2018-10-13 12:03:53 +0200

    Task1 updated

--- src/main/java/exercise2/Task1.java
+++ src/main/java/exercise2/Task1.java
@@ -1,27 +1,83 @@
 package exercise2;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Task1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter a number: ");
-        int fromuser= new Scanner(System.in).nextInt();
-        printEverySecond(fromuser);
+        int input = getFromuser();
+        if (checkIfInputIsIncorrect(input)) return;
+        printEverySecond(input);
+        printSequence2(input);
+        printSequence3(input);
+        //printSequence4(input);
+    }
+
+    //private static void printSequence4(int n) {    }
+
+    private static void printSequence3(int n) {
+        for (int i = 1, result = 2, multiplier = 1; i <= n; i++, result += 2 * multiplier) {
+            if (result == 8) {
+                multiplier = -1;
+            } else if (result == 2) {
+                multiplier = 1;
+            }
+            System.out.print(result + " ");
+        }
+        printLines(newLine());
+        int multiplier = 1;
+        System.out.print("2 ");
+        for (int i = 1, result = 4; i < n; i++, result += 2 * multiplier) {
+            if (i % 3 == 0) multiplier *= -1;
+            System.out.print(result + " ");
+        }
+        printLines(newLine());
+    }
+
+    private static String newLine() {
+        return "\n------------";
+    }
+
+    private static void printLines(String s) {
+        System.out.println(s);
+    }
+
+    private static void printSequence2(int n) {
+        for (int i = 1, k = -1, j = 1; i <= n; k += j, i++) {
+            j *= 2;
+            System.out.print(k + " ");
+        }
+        printLines(newLine());
     }
 
     private static void printEverySecond(int n) {
-        if (n >20){
-            System.out.println("Wrong number");
-        }else for (int i=1; i<=n; i+=2){
-            System.out.println(i);
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i * 2 - 1 + " ");
         }
+        printLines(newLine());
+        for (int i = 1; i <= 2 * n - 1; i += 2) {
+            System.out.print(i + " ");
+        }
+        printLines(newLine());
+        //LAMBDA WAY
+        IntStream.range(1, n + 1).forEach(t -> System.out.print(t * 2 - 1 + " "));
+        printLines(newLine());
     }
 
-    /*private static void printEverySecond() {
-        for (int i=1; i<=20; i+=2){
-            System.out.println(i);
+    private static boolean checkIfInputIsIncorrect(int fromuser) {
+        if (fromuser < 1 || fromuser > 20) {
+            printLines("Wrong number!!!");
+            return true;
         }
-    }*/
+        return false;
+    }
+
+    private static int getFromuser() {
+        printLines("Enter a number n: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+
 }
