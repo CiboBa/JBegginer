73b31f3f9cbfbce541f9acc28aabc51a516fcbfa
Bartek Cib <bciborowski.pl@gmail.com>
2018-10-14 15:13:28 +0200

    PalindromOccurrences committed

--- src/main/java/chains/StringOccurrences.java
+++ src/main/java/chains/StringOccurrences.java
@@ -1,5 +1,8 @@
 package chains;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,19 +14,57 @@ public class StringOccurrences {
         String text = getInputText();
         String key = getInputKey();
 
-        findAllOccurrences(text, key);
+        int[][] result = findAll(text, key);
+        print(result);
+        findAllIndexOf(text, key);
 
     }
 
-    private static void findAllOccurrences(String text, String key) {
+    private static List findAllIndexOf(String text, String key) {
+        System.out.println();
+        int index = 0;
+        List result = new ArrayList();
+        do {
+            index = text.indexOf(key, index);
+            if (index == -1) break;
+            result.add(Arrays.asList(index, index + key.length() - 1));
+            index++;
+        } while (true);
+
+        System.out.println(result);
+        return result;
+    }
+
+
+
+    private static void print(int[][] result) {
+        System.out.print("[");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print("{" + result[i][0] + " " + result[i][1] + "}");
+        }
+        System.out.print("]");
+    }
+
+    private static int[][] findAll(String text, String key) {
         Pattern pattern = Pattern.compile(key);
         Matcher matcher = pattern.matcher(text);
+
+        int counter = 0;
         while (matcher.find()) {
-            System.out.print("[{" + matcher.start() + "," + (matcher.end() - 1) + "}]");
-            System.out.println(matcher.group());
+            counter++;
         }
         matcher.reset();
+        int[][] occurrances = new int[counter][2];
+
+        for (int i = 0; i < counter; i++) {
+            matcher.find();
+            occurrances[i][0] = matcher.start();
+            occurrances[i][1] = matcher.end() - 1;
+        }
+
+        return occurrances;
     }
+
 
     private static String getInputKey() {
         System.out.println("Enter the key to find: ");
