01d595526d541763c2568dd64ce8c173d6f1afc2
Bartek Cib <bciborowski.pl@gmail.com>
2018-10-21 13:32:19 +0200

    FinalTest committed

---
+++ src/main/java/data_structures/static_final/FinalTest.java
@@ -0,0 +1,33 @@
+package data_structures.static_final;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FinalTest {
+
+    private String firstName;
+    private final String lastName;
+
+    public FinalTest(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String testMedthod() {
+        return "test";
+    }
+
+    private final String testFinalmethod() {
+        return "test final";
+    }
+
+    public static void main(String[] args) {
+        FinalTest object1 = new FinalTest("Nowacki");
+        object1.setFirstName("Karol");
+        object1.setFirstName("Żuk");
+
+        System.out.println(object1.testMedthod());
+        System.out.println(object1.testFinalmethod());
+    }
+}
--- src/main/java/data_structures/static_final/MainStaticTest.java
+++ src/main/java/data_structures/static_final/MainStaticTest.java
@@ -1,4 +1,4 @@
-package data_structures;
+package data_structures.static_final;
 
 public class MainStaticTest {
 
